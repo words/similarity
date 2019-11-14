92bb47422a2d7cffcb69cc7a4527023924681de6
Titus Wormer <tituswormer@gmail.com>
2019-11-14 13:19:11 +0100

    Update project

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var distance = require("leven")
+'use strict'
 
-module.exports = function(a,b) {
-  if (!a || !b || !a.length || !b.length) return 0
+var levenshtein = require('levenshtein-edit-distance')
+
+module.exports = similarity
+
+function similarity(a, b) {
+  if (!a || !b || a.length === 0 || b.length === 0) return 0
   if (a === b) return 1
-  var d = distance(a.toLowerCase(),b.toLowerCase())
+  var d = levenshtein(a.toLowerCase(), b.toLowerCase())
   var longest = Math.max(a.length, b.length)
-  return (longest-d)/longest
+  return (longest - d) / longest
 }
