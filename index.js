5310a6438cc44f04210d86a62578f434cc91d5d3
Titus Wormer <tituswormer@gmail.com>
2020-02-22 13:56:26 +0100

    Refactor algorithm

--- index.js
+++ index.js
@@ -5,17 +5,12 @@ var levenshtein = require('levenshtein-edit-distance')
 module.exports = similarity
 
 function similarity(a, b, options) {
-  var opts = options || {}
-  var sensitive = opts.sensitive
-  var left = casing(a || '', sensitive)
-  var right = casing(b || '', sensitive)
-  if (left === right) return 1
-  if (left.length === 0 || right.length === 0) return 0
-  var distance = levenshtein(left, right)
+  var left = a || ''
+  var right = b || ''
+  var insensitive = !(options || {}).sensitive
   var longest = Math.max(left.length, right.length)
-  return (longest - distance) / longest
-}
 
-function casing(value, sensitive) {
-  return sensitive ? value : value.toLowerCase()
+  return longest === 0
+    ? 1
+    : (longest - levenshtein(left, right, insensitive)) / longest
 }
